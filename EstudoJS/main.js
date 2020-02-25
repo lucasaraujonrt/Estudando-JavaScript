3e8555c135d2d75c5ea940335640968b5fc5bd0b
Lucas Araujo e Silva <lucasaraujo8186@gmail.com>
2020-02-25 17:10:52 -0300

    Promise

--- EstudoJS/main.js
+++ EstudoJS/main.js
@@ -1,11 +1,38 @@
-var xhr = new XMLHttpRequest(); // Funcionalidade do AJAX
+/*var xhr = new XMLHttpRequest(); // Funcionalidade do AJAX
 
 xhr.open("GET", "http://api.github.com/users/lucasaraujonrt"); // Buscando informacoes no GitHub
 xhr.send(null); // Paramentros enviados
 
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    // Verifica se a resposta do servidor voltou
-    console.log(JSON.parse(xhr.responseText)); // Convertendo para JSON
-  }
+xhr.onreadystatechange = function(){ 
+    if(xhr.readyState === 4){ // Verifica se a resposta do servidor voltou 
+        console.log(JSON.parse(xhr.responseText)); // Convertendo para JSON
+    }
+} */
+
+// PROMISES
+
+var mypromise = function() {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest(); // Funcionalidade do AJAX
+    xhr.open("GET", "http://api.github.com/users/lucasaraujonrt"); // Buscando informacoes no GitHub
+    xhr.send(null); // Paramentros enviados
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          // Sucesso com a requisicao
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject("Erro");
+        }
+      }
+    };
+  });
 };
+mypromise()
+  .then(function(response) {
+    console.log(response);
+  }) // Chama o resolve com o JSON como parametro
+  .catch(function(error) {
+    console.log(error);
+  }); // chama o reject com o erro
